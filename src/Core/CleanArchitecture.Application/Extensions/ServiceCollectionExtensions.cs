9fe964f04ace97886302bb6006954bc8779c4f33
mertguney <guneeymert@outlook.com>
2023-05-10 20:26:46 +0300

    Application Service Extension Created

---
+++ src/Core/CleanArchitecture.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddMediator();
+            services.AddAutoMapper();
+            services.AddValidators();
+        }
+
+        private static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+        private static void AddValidators(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        private static void AddMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
