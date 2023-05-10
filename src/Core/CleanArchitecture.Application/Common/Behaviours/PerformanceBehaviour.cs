ef0eef5f8ce7946a84f3fd09ee36400fc3864029
mertguney <guneeymert@outlook.com>
2023-05-10 20:27:16 +0300

    Logging And Performance Behaviour Created

---
+++ src/Core/CleanArchitecture.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace CleanArchitecture.Application.Common.Behaviours
+{
+    public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly Stopwatch _timer;
+        private readonly ILogger<TRequest> _logger;
+        private readonly ICurrentUserService _currentUserService;
+
+        public PerformanceBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+        {
+            _timer = new Stopwatch();
+            _logger = logger;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds > 500)
+            {
+                var requestName = typeof(TRequest).Name;
+                var userId = _currentUserService.UserId ?? string.Empty;
+                var userName = string.Empty;
+
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    //IdentityService Oluştur ve username getir. UserName değerine ata.
+                }
+                _logger.LogWarning("Long running request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@UserName} {@Request}", requestName, elapsedMilliseconds, userId, userName, request);
+            }
+            return response;
+        }
+    }
+}
