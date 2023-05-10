1856738d9f451f072e9c81fa4ab8ecf852e8fe5d
mertguney <guneeymert@outlook.com>
2023-05-10 20:26:29 +0300

    Infrastructure ServiceExtension Created

---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection services)
+        {
+            services.AddServices();
+        }
+
+        private static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+        }
+    }
+}
