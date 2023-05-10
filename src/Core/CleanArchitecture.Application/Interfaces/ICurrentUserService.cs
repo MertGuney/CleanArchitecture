41318ba5041fc182247ff3669b96a0e6eb611276
mertguney <guneeymert@outlook.com>
2023-05-10 20:25:50 +0300

    CurrentUserService Created

---
+++ src/Core/CleanArchitecture.Application/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Application.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        public string UserId { get; }
+    }
+}
---
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace CleanArchitecture.Infrastructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
